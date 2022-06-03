10497614635c3cdb21f4d6844c81a3f513029239
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-06-03 14:57:44 -0700

    Update doc2.ps1

--- doc2.ps1
+++ doc2.ps1
@@ -1,5 +1,10 @@
-﻿$dir1 = “D:\Desktop\Example1” #replace this with the destination of the source file (\\wab-ftepw01\usr\FIS\PDFStmts\*.pdf)
+$dir1 = “D:\Desktop\Example1” #replace this with the destination of the source file (\\wab-ftepw01\usr\FIS\PDFStmts\*.pdf)
 $dir2 = “D:\Desktop\Example2” #where ever the TRG files are located...
+
+$Username = "userNameofYourEmail";
+$Password = "passWordofYourEmail";
+
+$RESULTS = "D:\Desktop\Results.txt"
 
 #replace this with the *.pdf file extension. 
 $outputDirFile1 = get-ChildItem -Path $dir1 -Filter *.txt | ForEach-Object -Process {[System.IO.Path]::GetFileNameWithoutExtension($_)} 
@@ -7,12 +12,32 @@ $outputDirFile1 = get-ChildItem -Path $dir1 -Filter *.txt | ForEach-Object -Proc
 $outputDirFile2 = get-ChildItem -Path $dir2 -Filter *.rtf | ForEach-Object -Process {[System.IO.Path]::GetFileNameWithoutExtension($_)}
 
 #outputs the missing pdf file name (without extension)
-Compare-Object $outputDirFile2 $outputDirFile1 #will output the missing file
+Compare-Object $outputDirFile2 $outputDirFile1 | Out-File $RESULTS  #will output the missing file
 
-#if ($files -ne 0)
-#{
-#write-host "No files woops"
-#} 
+
+function Send-ToEmail([string]$email, [string]$attachmentpath){
+
+    $message = new-object Net.Mail.MailMessage;
+    $message.From = $Username;
+    $message.Subject = "Missing PDF for the following TRG file name";
+    $message.Body = "FIS MISSING STATEMENT PDF FILES";
+    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
+    $message.Attachments.Add($attachment);
+
+    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
+    $smtp.EnableSSL = $true;
+    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
+    $smtp.send($message);
+    write-host "Mail Sent" ; 
+    $attachment.Dispose();
+ }
+
+if ($files -ne 0) #if there are missing files populated, $files != 0
+{
+Write-Host "Missing files found. Sending txt file to FIS."
+Send-ToEmail  -email "personReceivingEmail@idk.com" -attachmentpath $RESULTS;
+
+} 
 
 
 
@@ -22,4 +47,5 @@ Compare-Object $outputDirFile2 $outputDirFile1 #will output the missing file
 
 
 
+                                                                                                    
                                                                                                     
