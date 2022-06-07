fa1d42eefed9d680427cb450e1aa037145a72825
Maya Islambouli <43270477+MayaIsla@users.noreply.github.com>
2022-06-07 11:34:43 -0500

    Update doc2.ps1

--- doc2.ps1
+++ doc2.ps1
@@ -15,35 +15,25 @@ $outputDirFile2 = get-ChildItem -Path $dir2 -Filter *.rtf | ForEach-Object -Proc
 Compare-Object $outputDirFile2 $outputDirFile1 | Out-File $RESULTS  #will output the missing file
 
 
-function Send-ToEmail([string]$email, [string]$attachmentpath){
-
-    $message = new-object Net.Mail.MailMessage;
-    $message.From = $Username;
-    $message.Subject = "Missing PDF for the following TRG file name";
-    $message.Body = "FIS MISSING STATEMENT PDF FILES";
-    $attachment = New-Object Net.Mail.Attachment($attachmentpath);
-    $message.Attachments.Add($attachment);
-
-    $smtp = new-object Net.Mail.SmtpClient("smtp.gmail.com", "587");
-    $smtp.EnableSSL = $true;
-    $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
-    $smtp.send($message);
-    write-host "Mail Sent" ; 
-    $attachment.Dispose();
- }
-
-if ($files -ne 0) #if there are missing files populated, $files != 0
+If ((Get-Content $RESULTS) -ne $Null) #checks if there are missing files populated
 {
 Write-Host "Missing files found. Sending txt file to FIS."
-Send-ToEmail  -email "personReceivingEmail@idk.com" -attachmentpath $RESULTS;
 
+
+$outlook = New-Object -ComObject Outlook.Application
+    $Mail = $outlook.CreateItem(0)
+    $Mail.to = "EmailofPersontosendto@blah.com"
+    $Mail.Subject = "Missing PDF for the following TRG file name"
+    $Mail.Body = "FIS MISSING STATEMENT PDF FILES."
+    $file = $RESULTS
+    $Mail.Attachments.Add($file)
+    $Mail.Send()
 } 
 
 else
 {
 Write-Host "No missing files found. Move on to next script."
 }
-
 
 
  
